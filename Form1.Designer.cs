9c79666d939c236ad6c6cfcd838810d1ad3db7db
Krzysztof Heppner <Krzysztof.Heppner1@gmail.com>
2022-09-04 23:08:31 +0200

    Dodaj pliki projektów.

---
+++ Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace AutomatycznyKlikacz1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.buttonOnOff = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 5;
+            this.timer2.Tick += new System.EventHandler(this.Timer2_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(144, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(37, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "1";
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Mnożnik (od 1 do 15):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(13, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(203, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "!!! Nie pisz z Numerycznej !!!";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(270, 134);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(54, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "0 - LPM";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "1 - PPM";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 117);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "2 - Enter";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(220, 137);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(47, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Odkliknij";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(188, 47);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(96, 13);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "razy około 63 = 63";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.LinkArea = new System.Windows.Forms.LinkArea(2, 6);
+            this.linkLabel1.LinkColor = System.Drawing.Color.Blue;
+            this.linkLabel1.Location = new System.Drawing.Point(15, 137);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(57, 17);
+            this.linkLabel1.TabIndex = 11;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "3 Własny -";
+            this.linkLabel1.UseCompatibleTextRendering = true;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "{SPACE}",
+            "{BACKSPACE}",
+            "{BREAK}",
+            "{CAPSLOCK}",
+            "{DELETE}",
+            "{DOWN}",
+            "{END}",
+            "{ENTER}",
+            "{ESC}",
+            "{HELP}",
+            "{HOME}",
+            "{INSERT}",
+            "{LEFT}",
+            "{NUMLOCK}",
+            "{PGDN}",
+            "{PGUP}",
+            "{PRTSC}",
+            "{RIGHT}",
+            "{SCROLLLOCK}",
+            "{TAB}",
+            "{UP}",
+            "{F1}",
+            "{F2}",
+            "{F3}",
+            "{F4}",
+            "{F5}",
+            "{F6}",
+            "{F7}",
+            "{F8}",
+            "{F9}",
+            "{F10}",
+            "{F11}",
+            "{F12}",
+            "{F13}",
+            "{F14}",
+            "{F15}",
+            "{F16}",
+            "{ADD}",
+            "{SUBTRACT}",
+            "{MULTIPLY}",
+            "{DIVIDE}"});
+            this.comboBox1.Location = new System.Drawing.Point(78, 133);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(103, 21);
+            this.comboBox1.TabIndex = 12;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.TextUpdate += new System.EventHandler(this.comboBox1_TextUpdate);
+            // 
+            // buttonOnOff
+            // 
+            this.buttonOnOff.Location = new System.Drawing.Point(223, 87);
+            this.buttonOnOff.Name = "buttonOnOff";
+            this.buttonOnOff.Size = new System.Drawing.Size(101, 33);
+            this.buttonOnOff.TabIndex = 13;
+            this.buttonOnOff.Text = "Wyłącz";
+            this.buttonOnOff.UseVisualStyleBackColor = true;
+            this.buttonOnOff.Click += new System.EventHandler(this.buttonOnOff_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(336, 163);
+            this.Controls.Add(this.buttonOnOff);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.MaximumSize = new System.Drawing.Size(352, 202);
+            this.MinimumSize = new System.Drawing.Size(352, 202);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button buttonOnOff;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace AutomatycznyKlikacz1
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+            timer2.Start();
+            Thread mainThread = Thread.CurrentThread;
+            mainThread.Name = "Main Thread";
+        }
+
+        int mnoznik = 1;
+        bool numPad0 = false; bool czekanieNumPad0 = false;
+        bool numPad1 = false; bool czekanieNumPad1 = false;
+        bool numPad2 = false; bool czekanieNumPad2 = false;
+        bool numPad3 = false; bool czekanieNumPad3 = false;
+        string znak = " ";
+
+
+        public void Klikanie(string a)
+        {
+            for (int i = 0; i < mnoznik; i++)
+            {
+                switch (a)
+                {
+                    case "NumPad0":
+                        uint XL = (uint)System.Windows.Forms.Cursor.Position.X;
+                        uint YL = (uint)System.Windows.Forms.Cursor.Position.Y;
+                        mouse_event(0x02 | 0x04, XL, YL, 0, 0);
+                        break;
+                    case "NumPad1":
+                        uint XR = (uint)System.Windows.Forms.Cursor.Position.X;
+                        uint YR = (uint)System.Windows.Forms.Cursor.Position.Y;
+                        mouse_event(0x06 | 0x08, XR, YR, 0, 0);
+                        break;
+                    case "NumPad2":
+                        SendKeys.Send("{ENTER}");
+                        break;
+                    case "NumPad3":
+                        try
+                        {
+                            SendKeys.Send(znak);
+                        }
+                        catch
+                        {
+                            znak = " ";
+                            MessageBox.Show("Podany znak jest błędny, zmieniono na SPACE", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        public void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (numPad0)
+            {
+                Klikanie("NumPad0");
+            }
+            if (numPad1)
+            {
+                Klikanie("NumPad1");
+            }
+            if (numPad2)
+            {
+                Klikanie("NumPad2");
+            }
+            if (numPad3)
+            {
+                Klikanie("NumPad3");
+            }
+        }
+
+        public void Timer2_Tick(object sender, EventArgs e)
+        {
+            //Numpad 0
+            if (((Keyboard.GetKeyStates(Key.NumPad0) & KeyStates.Down) > 0) && (czekanieNumPad0 == false))
+            {
+                numPad0 = !numPad0;
+                czekanieNumPad0 = true;
+                //if((textBox1.Text == "6") && (thread1.IsAlive == false))
+                //    thread1.Start();
+            }
+            else if ((Keyboard.GetKeyStates(Key.NumPad0) & KeyStates.Down) == 0)
+            {
+                czekanieNumPad0 = false;
+                //if ((textBox1.Text == "6") && (thread1.IsAlive == true))
+                //    thread1.Interrupt();
+            }
+
+            //Numpad 1
+            if (((Keyboard.GetKeyStates(Key.NumPad1) & KeyStates.Down) > 0) && (czekanieNumPad1 == false))
+            {
+                numPad1 = !numPad1;
+                czekanieNumPad1 = true;
+            }
+            else if ((Keyboard.GetKeyStates(Key.NumPad1) & KeyStates.Down) == 0)
+            {
+                czekanieNumPad1 = false;
+            }
+
+            //Numpad 2
+            if (((Keyboard.GetKeyStates(Key.NumPad2) & KeyStates.Down) > 0) && (czekanieNumPad2 == false))
+            {
+                numPad2 = !numPad2;
+                czekanieNumPad2 = true;
+            }
+            else if ((Keyboard.GetKeyStates(Key.NumPad2) & KeyStates.Down) == 0)
+            {
+                czekanieNumPad2 = false;
+            }
+
+            //Numpad 3
+            if (((Keyboard.GetKeyStates(Key.NumPad3) & KeyStates.Down) > 0) && (czekanieNumPad3 == false))
+            {
+                numPad3 = !numPad3;
+                czekanieNumPad3 = true;
+            }
+            else if ((Keyboard.GetKeyStates(Key.NumPad3) & KeyStates.Down) == 0)
+            {
+                czekanieNumPad3 = false;
+            }
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                int.TryParse(textBox1.Text, out int otrzymanyMnoznik);
+                if ((otrzymanyMnoznik > 0) && otrzymanyMnoznik < 16)
+                {
+                    mnoznik = otrzymanyMnoznik;
+                    label8.Text = "razy około 63 = " + mnoznik * 63;
+                    SendKeys.Send("{TAB}");
+                }
+                else
+                {
+                    MessageBox.Show("za duża lub za mała liczba", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    mnoznik = 1;
+                    textBox1.Text = "1";
+                }
+            }
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.sendkeys?view=windowsdesktop-5.0#remarks");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (comboBox1.Text[0].ToString() != "{")
+                {
+                    comboBox1.Text = "{" + comboBox1.Text;
+                }
+                if (comboBox1.Text[comboBox1.Text.Length - 1].ToString() != "}")
+                {
+                    comboBox1.Text = comboBox1.Text + "}";
+                }
+                znak = comboBox1.Text;
+            }
+            catch { }
+        }
+
+        private void comboBox1_TextUpdate(object sender, EventArgs e)
+        {
+            comboBox1_SelectedIndexChanged(null, null);
+        }
+
+        private void buttonOnOff_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled == false)
+            {
+                timer1.Start();
+                timer2.Start();
+                buttonOnOff.Text = "Wyłącz";
+            }
+            else
+            {
+                timer1.Stop();
+                timer2.Stop();
+                buttonOnOff.Text = "Włącz";
+            }
+        }
+    }
+}
